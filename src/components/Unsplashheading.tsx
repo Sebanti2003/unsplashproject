599f34429c06bcc40abc302d99ca1243d32ec5ce
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-08-02 02:03:14 +0530

    Eliminated unnessessary stuffs

--- src/components/Unsplashheading.tsx
+++ src/components/Unsplashheading.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const Unsplashheading = () => {
   return (
-    <div className='font-bold text-5xl my-3 dark:text-white text-blue-600 text-center capitalize'>
+    <div className="font-bold text-5xl my-3 dark:text-white text-blue-600 text-center capitalize">
       unsplash photo gallery
     </div>
-  )
-}
+  );
+};
 
-export default Unsplashheading
+export default Unsplashheading;
