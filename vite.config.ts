a41f8015dc936f7ebadf3eb518f5545798dafcb7
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-08-02 01:09:52 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  env: {
-    'VITE_API_URL': process.env.VITE_API_URL,
-    'VITE_API_TOKEN': process.env.VITE_API_TOKEN,
-  },
-}as any)
+})
